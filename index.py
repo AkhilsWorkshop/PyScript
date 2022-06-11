0efdcc02ff42901108cb9a6515d2e346e3ff1650
Akhil Kumar Harikumar <67311174+AkhilsWorkshop@users.noreply.github.com>
2022-06-10 22:24:54 -0500

    external pyScript

---
+++ index.py
@@ -0,0 +1,9 @@
+import random
+
+greetings = ["Hola","Hello","Vanakkam","Namasthe","Welcome","Hey"]
+name = ["coder","problem solver","gamer","user"]
+
+result1 = random.choice(greetings)
+result2 = random.choice(name)
+
+print(result1 + " " + result2)
